b3a8d39c4727dd61352e8af8f837728e3a9cf29b
edwinsn <edwinsanchez750@gmail.com>
2022-09-24 19:00:48 -0500

    Moved the vertical scroll component to be below the previous horizontal component end

--- components/VerticalScroll.js
+++ components/VerticalScroll.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import Scrolleable from './Scrolleable';
+import { useDimentions } from './DimentionsContext'
 
-export default function VerticalScroll({ children, position }) {
+export default function VerticalScroll({ children, position, style }) {
+
+  const { dimentions } = useDimentions()
+
+  const previousComponentsHorizontalWidths = dimentions
+    .filter(({ orientation }) => orientation === 'horizontal')
+    .slice(0, position)
+    .reduce((acc, { width }) => acc + width, 0)
+
+  const positionStyle = {
+    position: 'relative',
+    left: (previousComponentsHorizontalWidths - dimentions[position]?.width) || 0,
+  }
+
+  positionStyle.left = positionStyle.left < 0 ? 0 : positionStyle.left
 
   return (
     <Scrolleable
+      style={{ ...style, ...positionStyle }}
       position={position}
       orientation="vertical"
       className='HV-scroll-vertical-container'
